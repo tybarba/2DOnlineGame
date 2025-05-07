9776f0ee53f5c63f36c29b36b44eb585fecee750
Tyler B <tyler.barba2023@my.ntu.ac.uk>
2025-05-07 23:00:12 +0100

    coin spawning

--- Assets/Scripts/Core/Coins/RespawningCoin.cs
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
+using System;
 
 public class RespawningCoin : Coin
 {
+    private Vector3 previousPosition;
+    public event Action<RespawningCoin> OnCollected;
+
+    void Start()
+    {
+        previousPosition = this.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        if(IsServer) return;
+
+        if(gameObject.transform.position != previousPosition){
+            Show(true);
+            previousPosition = gameObject.transform.position;
+        }
+    }
     public override int Collect()
     {
         if(!IsServer){
@@ -12,6 +30,16 @@ public class RespawningCoin : Coin
         if(alreadyCollected) return 0;
 
         alreadyCollected = true;
+
+        OnCollected?.Invoke(this);
+
         return coinValue;
     }
+
+    public void Reset()
+    {
+        alreadyCollected = false;
+    }
+
+    
 }
